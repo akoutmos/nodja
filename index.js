9a3291a7c77a428f126cd5148ec8d20d1dbfcdf8
Alex Koutmos <alexander.koutmos@zonarsystems.com>
2016-06-16 16:05:44 -0700

    CLI complete, templates complete

--- index.js
+++ index.js
@@ -1,13 +1,65 @@
-let cli = require('commander'),
+let fs = require('fs'),
+    commander = require('commander'),
     glob = require('glob'),
     which = require('which');
 
-exports.cli = function (args) {
-    cli
+//Exposed module functions
+exports.cli = cli;
+
+/**
+ * Returns the contents of a given file synchronously
+ *
+ * @param path
+ * @returns {String}
+ */
+function getFileContents (path) {
+    return fs.readFileSync(path).toString();
+}
+
+/**
+ * The cli entry point
+ *
+ * @param args the command line arguments
+ */
+function cli (args) {
+    //Get ninja templates
+    let templates_path = `${__dirname}/templates`,
+        templates = Object.freeze({
+            'build_statement': getFileContents(`${templates_path}/build_statement`),
+            'default': getFileContents(`${templates_path}/default`),
+            'ninja': getFileContents(`${templates_path}/ninja`),
+            'rule': getFileContents(`${templates_path}/rule`),
+            'variable': getFileContents(`${templates_path}/variable`)
+        });
+
+    //Configure CLI options via commander
+    commander
+        .command('generate <input_file>')
+        .alias('g')
+        .description('generate a ninja build file from the provided nodja config (written to build.ninja if output_file is not provided)')
+        .option('--output <output_file>', 'where to write the output ninja file to')
+        .option('--run', 'if provided, created ninja file will be executed')
+        .action(function (input_file, options) {
+            console.log(input_file);
+            console.log(options.output);
+            console.log(options.run);
+        });
+
+    commander
+        .command('check <input_file>')
+        .alias('c')
+        .description('validate the provided nodja build file')
+        .action(function (input_file) {
+
+        });
+
+    commander
         .version('0.0.2')
-        .option('-g, --generate <input_file> [output_file]', 'Generate a Ninja build file from the provided nodja config (written to build.ninja if output_file is not provided)')
-        .option('-r, --run [task]', 'If provided during --generate, created Ninja file will be executed')
-        .option('-c, --check <file>', 'Validate the provided nodja file')
-        .option('-h, --help', 'Print this menu')
         .parse(args);
-};
+
+
+    //If no arguments passed in, print help and exit
+    if (!commander.args.length){
+        commander.help();
+    }
+}
