74e1e4da0b349004df255382b3d917ceed17bc10
Alex Koutmos <alexander.koutmos@zonarsystems.com>
2016-06-16 21:44:30 -0700

    All working minus build statement section

--- index.js
+++ index.js
@@ -34,6 +34,51 @@ function fileExists (file) {
 }
 
 /**
+ * Returns whether the provided file is a valid nodja config
+ *
+ * @param config
+ * @returns {Boolean}
+ */
+function validateNodjaConfig (config) {
+    let config_contents,
+        nodja_config;
+
+    //Check to make sure file exists
+    if (!fileExists(config)) {
+        console.error(`ERROR: Could not find '${config}'`);
+        return false;
+    }
+
+    //Check that it is a valid JSON file
+    try {
+        config_contents = fs.readFileSync(config).toString();
+        nodja_config = JSON.parse(config_contents);
+    } catch (err) {
+        console.error('ERROR: Nodja configuration is malformed JSON');
+        return false;
+    }
+
+    //Check that the schema is correct
+    if (typeof nodja_config.variables !== 'object') {
+        console.error('ERROR: \'variables\' entry must be an object');
+        return false;
+    }
+
+    if (typeof nodja_config.rules !== 'object') {
+        console.error('ERROR: \'rules\' entry must be an object');
+        return false;
+    }
+
+    if (!Array.isArray(nodja_config.build_statements)) {
+        console.error('ERROR: \'build_statements\' entry must be an array');
+        return false;
+    }
+
+    //If we got here, it is valid
+    return true;
+}
+
+/**
  * The cli entry point
  *
  * @param args the command line arguments
@@ -57,8 +102,54 @@ function cli (args) {
         .option('--output <output_file>', 'where to write the output ninja file to')
         .option('--run', 'if provided, created ninja file will be executed')
         .action(function (input_file, options) {
+            //Set option defaults if none provided and get base ninja template
             let run_build = options.run || false,
-                output_file = options.output || 'build.ninja';
+                output_file_name = options.output || 'build.ninja',
+                output_ninja_file = templates.ninja,
+                nodja_config,
+                variable_section = '',
+                rule_section = '';
+
+            //Validate nodja config
+            if ( !validateNodjaConfig(input_file)) {
+                console.error('An error occured! Aborting.');
+                process.exit(1);
+            }
+
+            //Read in file
+            nodja_config = JSON.parse(fs.readFileSync(input_file).toString());
+
+            //Set variable section
+            for (let variable in nodja_config.variables) {
+                let entry = templates.variable;
+                entry = entry.replace('{{name}}', variable);
+                entry = entry.replace('{{value}}', nodja_config.variables[variable]);
+
+                variable_section += entry;
+            }
+            output_ninja_file = output_ninja_file.replace('{{variables}}', variable_section);
+
+            //Set rules section
+            for (let rule in nodja_config.rules) {
+                let entry = templates.rule;
+                entry = entry.replace('{{name}}', rule);
+                entry = entry.replace('{{command}}', nodja_config.rules[rule]);
+
+                rule_section += entry;
+            }
+            output_ninja_file = output_ninja_file.replace('{{rules}}', rule_section);
+
+
+            //Set build statements section
+
+
+            //Write out generated ninja build file
+            try {
+                fs.writeFileSync(output_file_name, output_ninja_file);
+            } catch (err) {
+                console.error(`ERROR: Could not write to '${output_file_name}'`);
+                process.exit(1);
+            }
 
             //If the build should be executed
             if (run_build) {
@@ -66,12 +157,12 @@ function cli (args) {
                 try {
                     which.sync('ninja');
                 } catch (err) {
-                    console.err('ERROR: ninja binary not found');
+                    console.error('ERROR: ninja binary not found');
                     process.exit(1);
                 }
 
                 //Spawn child ninja process, detach and exit successfully
-                child_process.spawn('ninja', ['-f', output_file], {
+                child_process.spawn('ninja', ['-f', output_file_name], {
                     cwd: process.cwd(),
                     env: process.env,
                     stdio: 'inherit',
