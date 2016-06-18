deddaac6382062989d047958b61dda6e7bab92a2
Alex Koutmos <alexander.koutmos@zonarsystems.com>
2016-06-18 10:55:45 -0700

    Added more checks to schema validator
    Specifically deep checking on rules

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+//Lib imports
 let fs = require('fs'),
     child_process = require('child_process'),
     commander = require('commander'),
     glob = require('glob'),
     which = require('which');
+
+//Module constants
+const templates_path = `${__dirname}/templates`;
+const templates = Object.freeze({
+    'build_statement': getFileContents(`${templates_path}/build_statement`),
+    'default': getFileContents(`${templates_path}/default`),
+    'ninja': getFileContents(`${templates_path}/ninja`),
+    'rule': getFileContents(`${templates_path}/rule`),
+    'variable': getFileContents(`${templates_path}/variable`)
+});
+
 
 //Exposed module functions
 exports.cli = cli;
@@ -58,17 +70,54 @@ function validateNodjaConfig (config) {
         return false;
     }
 
-    //Check that the schema is correct
-    if (typeof nodja_config.variables !== 'object') {
+    //Check that variables schema is correct
+    if (!nodja_config.hasOwnProperty('variable_sets') && !nodja_config.hasOwnProperty('variables')) {
+        console.error(`ERROR: '${config}' must have either a 'variables' or 'variable_sets' entry`);
+        return false;
+    }
+
+    if (nodja_config.hasOwnProperty('variable_sets') && nodja_config.hasOwnProperty('variables')) {
+        console.error(`ERROR: '${config}' cannot contain both 'variables' and 'variable_sets' entries`);
+        return false;
+    }
+
+    if (!nodja_config.hasOwnProperty('variable_sets') && nodja_config.hasOwnProperty('variables') && typeof nodja_config.variables !== 'object') {
         console.error('ERROR: \'variables\' entry must be an object');
         return false;
     }
 
-    if (typeof nodja_config.rules !== 'object') {
+    if (nodja_config.hasOwnProperty('variables')) {
+        for (let variable in nodja_config.variables) {
+            if (typeof nodja_config.variables[variable] !== 'string') {
+                console.error(`ERROR: Variable with key \'${variable}\' must be a string`);
+                return false;
+            }
+        }
+    }
+
+    if (nodja_config.hasOwnProperty('variable_sets')) {
+        for (let set in nodja_config.variable_sets) {
+            if (typeof nodja_config.variable_sets[set] !== 'object') {
+                console.error(`ERROR: Variable with key \'${set}\' must be an object`);
+                return false;
+            }
+
+            for (let variable in nodja_config.variable_sets[set]) {
+                if (typeof nodja_config.variable_sets[set][variable] !== 'string') {
+                    console.error(`ERROR: Variable with key \'${variable}\' in variable set '${set}' must be a string`);
+                    return false;
+                }
+            }
+        }
+    }
+
+    //Check that rules schema is correct
+    if (!nodja_config.hasOwnProperty('rules') && nodja_config.hasOwnProperty('rules') && typeof nodja_config.rules !== 'object') {
         console.error('ERROR: \'rules\' entry must be an object');
         return false;
     }
 
+    //Check that build statements schema is correct
     if (!Array.isArray(nodja_config.build_statements)) {
         console.error('ERROR: \'build_statements\' entry must be an array');
         return false;
@@ -78,22 +127,83 @@ function validateNodjaConfig (config) {
     return true;
 }
 
+
+
+/**
+ * Create a variable assignment entry for ninja config
+ *
+ * @param name
+ * @param value
+ * @returns {String}
+ */
+function generateVariableEntry (name, value) {
+    let entry = templates.variable;
+
+    entry = entry.replace('{{name}}', name);
+    entry = entry.replace('{{value}}', value);
+
+    return entry;
+}
+
+/**
+ * Create a rule assignment entry for the ninja config
+ *
+ * @param rule
+ * @param command
+ * @returns {undefined}
+ */
+function generateRuleEntry (rule, command) {
+    let entry = templates.rule;
+
+    entry = entry.replace('{{name}}', rule);
+    entry = entry.replace('{{command}}', command);
+
+    return entry;
+}
+
+/**
+ * Create a build statement entry for the ninja config
+ *
+ * @param output
+ * @param rule
+ * @param input
+ * @returns {String}
+ */
+function generateBuildStatementEntry (output, rule, input) {
+    let entry = templates.build_statement;
+
+    entry = entry.replace('{{output}}', output);
+    entry = entry.replace('{{rule}}', rule);
+    entry = entry.replace('{{dependencies}}', input);
+
+    return entry;
+}
+
+/**
+ * Create a default entry statement
+ *
+ * @param output
+ * @returns {String}
+ */
+function generateDefaultEntry (output) {
+    let entry = templates['default'];
+
+    entry = entry.replace('{{build_statement}}', output);
+
+    return entry;
+}
+
+function generatePhonyEntry () {
+
+}
+
+
 /**
  * The cli entry point
  *
  * @param args the command line arguments
  */
 function cli (args) {
-    //Get ninja templates
-    let templates_path = `${__dirname}/templates`,
-        templates = Object.freeze({
-            'build_statement': getFileContents(`${templates_path}/build_statement`),
-            'default': getFileContents(`${templates_path}/default`),
-            'ninja': getFileContents(`${templates_path}/ninja`),
-            'rule': getFileContents(`${templates_path}/rule`),
-            'variable': getFileContents(`${templates_path}/variable`)
-        });
-
     //Configure CLI options via commander
     commander
         .command('generate <input_file>')
@@ -107,8 +217,11 @@ function cli (args) {
                 output_file_name = options.output || 'build.ninja',
                 output_ninja_file = templates.ninja,
                 nodja_config,
-                variable_section = '',
-                rule_section = '';
+                num_build_statements = 0,
+                variable_section = [],
+                rule_section = [],
+                build_statement_section = [],
+                default_build_statements = [];
 
             //Validate nodja config
             if ( !validateNodjaConfig(input_file)) {
@@ -121,27 +234,64 @@ function cli (args) {
 
             //Set variable section
             for (let variable in nodja_config.variables) {
-                let entry = templates.variable;
-                entry = entry.replace('{{name}}', variable);
-                entry = entry.replace('{{value}}', nodja_config.variables[variable]);
-
-                variable_section += entry;
+                variable_section.push(
+                    generateVariableEntry(variable, nodja_config.variables[variable])
+                );
             }
-            output_ninja_file = output_ninja_file.replace('{{variables}}', variable_section);
+
 
             //Set rules section
             for (let rule in nodja_config.rules) {
-                let entry = templates.rule;
-                entry = entry.replace('{{name}}', rule);
-                entry = entry.replace('{{command}}', nodja_config.rules[rule]);
-
-                rule_section += entry;
+                rule_section.push(
+                    generateRuleEntry(rule, nodja_config.rules[rule])
+                );
             }
-            output_ninja_file = output_ninja_file.replace('{{rules}}', rule_section);
 
 
-            //Set build statements section
 
+
+
+            //Set build statements section and defaults
+            num_build_statements = nodja_config.build_statements.length;
+            for (let i = 0; i < num_build_statements; i++) {
+                let entry = templates.build_statement,
+                    build_statement = nodja_config.build_statements[i];
+
+                //Output has exactly 1 input dependency (1:1)
+                if (!glob.hasMagic(build_statement.output) && !glob.hasMagic(build_statement.input)) {
+                    //Append resulting build step
+                    build_statement_section.push(
+                        generateBuildStatementEntry(build_statement.output, build_statement.rule, build_statement.input)
+                    );
+                }
+
+                //Multiple inputs have been detected via globbing
+                else if (!glob.hasMagic(build_statement.output) && glob.hasMagic(build_statement.input)) {
+                    let inputs = glob.sync(build_statement.input);
+
+                    //Append resulting build step
+                    build_statement_section.push(
+                        generateBuildStatementEntry(build_statement.output, build_statement.rule, inputs.join(' '))
+                    );
+                }
+
+                //Add build statement to list of defaults if flag set
+                if (build_statement.hasOwnProperty('default') && build_statement['default']) {
+                    default_build_statements.push(
+                        generateDefaultEntry(build_statement.output)
+                    );
+                }
+            }
+
+
+            //TODO: Set phony statements section
+
+
+            //Set all aggregated sections
+            output_ninja_file = output_ninja_file.replace('{{variables}}', variable_section.join('\n'));
+            output_ninja_file = output_ninja_file.replace('{{rules}}', rule_section.join('\n'));
+            output_ninja_file = output_ninja_file.replace('{{build_statements}}', build_statement_section.join('\n'));
+            output_ninja_file = output_ninja_file.replace('{{defaults}}', default_build_statements.join('\n'));
 
             //Write out generated ninja build file
             try {
@@ -162,13 +312,14 @@ function cli (args) {
                 }
 
                 //Spawn child ninja process, detach and exit successfully
-                child_process.spawn('ninja', ['-f', output_file_name], {
+                let ninja = child_process.spawnSync('ninja', ['-f', output_file_name], {
                     cwd: process.cwd(),
                     env: process.env,
-                    stdio: 'inherit',
-                    detached: true
-                }).unref();
-                process.exit(0);
+                    stdio: 'inherit'
+                });
+
+                //Return the ninja process exit code
+                process.exit(ninja.status);
             }
         });
 
